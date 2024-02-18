6e51d0ef23a5a94985a09366b364bd589fddb600
Jami Räsänen <jamirasanen12@gmail.com>
2024-02-18 20:23:15 +0200

    put request for recipe. Made get requests for a recipes ingredients, keywords, reviews and images. Made a post request for adding keywords to a recipe

--- backend/RecipeAppBackend/RecipeAppBackend/Interfaces/IRecipeRepository.cs
+++ backend/RecipeAppBackend/RecipeAppBackend/Interfaces/IRecipeRepository.cs
@@ -12,9 +12,11 @@ namespace RecipeAppBackend.Interfaces
         ICollection<Keyword> GetKeywordsOfRecipe(int id);
         Decimal GetRating(int id);
         int GetFavoriteCount(int id);
-
         bool RecipeExists(int id);
+        bool AddKeyword(RecipeKeyword recipeKeyword);
+        bool KeywordExists(int recipeId, int keywordId);
         bool CreateRecipe(Recipe recipe, List<RecipeKeyword> recipeKeywords);
+        bool UpdateRecipe(Recipe recipe);
         bool Save(); 
     }
 }
