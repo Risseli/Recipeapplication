4df7d6988ca2e776d827de08d140d1d809b1f5ac
Riku Ovaskainen <127824140+Risseli@users.noreply.github.com>
2024-03-14 08:14:11 +0200

    Added Add review to recipeDetails with star rating feature. Added new component StarReview to Components. Have to install npm "npm install react-icons --save" for this to wo

--- reseptisovellus/src/Components/StarRating.js
+++ reseptisovellus/src/Components/StarRating.js
@@ -1,10 +1,7 @@
 import { React, useState } from "react";
-//import "./StarRating.css";
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating({rating, setRating, rateColor, setColor}) {
-  //const [rating, setRating] = useState(null);
-  //const [rateColor, setColor] = useState(null);
   return (
     <>
       {[...Array(5)].map((star, index) => {
